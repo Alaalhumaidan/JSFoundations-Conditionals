19ea6bedbb81df8036bb75a5fdb7a60b8ea00946
Alaalhumaidan <Alaalhumaidan@gmail.com>
2021-05-25 04:25:14 +0300

    Solved the conditionals task

---
+++ CALC.js
@@ -0,0 +1,5 @@
+let num1 = 3;
+let num2 = 6;
+console.log(`Please enter the first number: ${ num1 }`);
+console.log(`Please enter the second number: ${ num2 }`);
+console.log(` ${num1} * ${num2} = ${num1*num2}`);
